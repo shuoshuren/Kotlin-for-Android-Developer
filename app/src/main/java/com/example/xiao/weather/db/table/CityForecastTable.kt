a0977151bd4cdf2a16fb944a6c9db867de877095
shuoshuren <534361124@qq.com>
2017-05-27 14:34:35 +0800

    s实现一个SQLiteHelper

---
+++ app/src/main/java/com/example/xiao/weather/db/table/CityForecastTable.kt
@@ -0,0 +1,14 @@
+package com.example.xiao.weather.db.table
+
+/**
+ * 城市信息的表
+ *
+ * Created by xiao on 2017/5/27.
+ */
+
+object CityForecastTable{
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
---
+++ app/src/main/java/com/example/xiao/weather/db/table/DayForecastTable.kt
@@ -0,0 +1,18 @@
+package com.example.xiao.weather.db.table
+
+/**
+ * 某天的天气预报
+ *
+ * Created by xiao on 2017/5/27.
+ */
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
